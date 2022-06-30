3785bac3fcea0efc7767190437011abb5b23d67c
Kevin510139 <83684184+Kevin510139@users.noreply.github.com>
2022-06-30 15:25:01 +0800

    Add files via upload

--- Wta/main.py
+++ Wta/main.py
@@ -1,17 +1,18 @@
 import time
 import numpy as np
 from src.model import Q_learning
-from src.utils import load_sink_data, write_reult, compute_all_tar, compute_value_of_q_table, load, trace_progress
+from src.utils import load_greedy_ans, load_sink_data, write_reult, compute_all_tar, compute_value_of_q_table, load, trace_progress
 
 
 def main():
-    EPOCHS = 5000
+    EPOCHS = 3000
     LEARNING_RATE = 0.2
-    GAMMA = 0.9
+    GAMMA = 0.95
     EPSILON = 0.1
     start = time.time()
     tar, weapon = load()
     sink_data = load_sink_data()
+    data_ans = float(load_greedy_ans())
     Q_table = np.zeros((5,7))
     Q_table, value_best, value_after = Q_learning(
         Q_table,
@@ -24,19 +25,19 @@ def main():
         e = EPOCHS
     )
     trace_progress(
-        value_best,
-        6.973920131546892,
+        value_after,
+        data_ans,
         f"Wta_exploration"
     )
     trace_progress(
-        value_after,
-        6.973920131546892,
+        value_best,
+        data_ans,
         f"Wta_solution"
     )
-    greedy_tar = compute_all_tar(Q_table, weapon, tar, sink_data)
-    greedy_value = compute_value_of_q_table(Q_table, tar, weapon, sink_data)
+    greedy_value,greedy_tar = compute_value_of_q_table(Q_table, tar, weapon, sink_data)
     write_reult(greedy_tar,greedy_value,Q_table)
     print(f"Time to run : {round(time.time() - start, 3)}")
+
     
     
 if __name__ == "__main__":
